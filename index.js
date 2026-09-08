2915fe044599c1cd398ab2fa82643e00c3aacc7a
77665645abohashim-ai <77665645abohashim@gmail.com>
2026-09-08 14:34:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3247,29 +3247,46 @@ app.get('/api/v2/topics/list', (req, res) => {
             "user_member": null
           },
           {
-            "id": 36469,
-            "commentable_id": 54935,
-            "commentable_type": "App\\Model\\ThinkPost",
-            "user_id": 3326447,
-            "content": "How can we use an older version of the software? ",
-            "img": "",
-            "thumb_count": 0,
-            "time": 1781692836,
-            "user": {
-              "id": 3326447,
-              "name": "moham6e6",
-              "avatar": ""
-            },
-            "is_thumb": null,
-            "user_member": null
-          }
-          
-            }
-        ]
-      }
+            "id": 36467,
+                    "commentable_id": 54935,
+                    "commentable_type": "App\\Model\\ThinkPost",
+                    "user_id": 3591729,
+                    "content": "pls my thinkdiag pro was working perfectly before my subscription got expired and was blocked but after I paid for the registration and reopened, it's stop communicating to vehicles ",
+                    "img": "",
+                    "thumb_count": 0,
+                    "time": 1781691153,
+                    "user": {
+                        "id": 3591729,
+                        "name": "engineerefetoboreaut",
+                        "avatar": "https://file.thinkcar.com/app_services/2023-08-16/20230816221443475649.png"
+                    },
+                    "is_thumb": null,
+                    "user_member": {
+                        "id": 94873,
+                        "user_id": 3591729,
+                        "expiration_time": 1813149336,
+                        "is_perpetual": 0
+                    }
+                },
+                {
+                    "id": 36421,
+                    "commentable_id": 54935,
+                    "commentable_type": "App\\Model\\ThinkPost",
+                    "user_id": 5265251,
+                    "content": "The ",
+                    "img": "",
+                    "thumb_count": 0,
+                    "time": 1781194236,
+                    "user": {
+                        "id": 5265251,
+                        "name": "Afamaco2026",
+                        "avatar": ""
+                    }
+                }
+            ]
+        }
     });
 });
-
 
 app.use((req, res) => {
     res.status(404).json({
@@ -3280,4 +3297,4 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
-});
+}); 
