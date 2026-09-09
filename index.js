40a7636c4217e34286c20d92659be0d374928a84
77665645abohashim-ai <77665645abohashim@gmail.com>
2026-09-09 18:50:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const carData = require('./car_data.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
